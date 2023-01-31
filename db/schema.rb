1eeb3d3b238bb68672be01c6452a4a041c6a73c9
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-31 11:53:40 +0900

    Postモデルの作成のし直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_022306) do
+ActiveRecord::Schema.define(version: 2023_01_31_023716) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
