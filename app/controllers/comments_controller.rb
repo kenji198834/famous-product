69c31b03cdd9de0f0698c9c79f1c7c6313e9b162
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-01-31 21:10:30 +0900

    コメント機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,18 @@
 class CommentsController < ApplicationController
 
-def create
-end
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      render "posts/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 
 end
