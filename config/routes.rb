0a19429f6d4136f6eeedf69bbc9040ec851e47a1
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-01 16:11:44 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
-  root to: "posts#index"
+  root 'users#index'
   resources :posts do
     resources :comments, only: [:create]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 end
