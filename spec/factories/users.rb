6a747f05915ec0769bdae0f02cd05572c3d69455
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-02 17:23:35 +0900

    FactoryBotの記述を編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    product               {Faker::Name.initials(number: 2)}
   end
 end
