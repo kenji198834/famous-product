f3d244d72a2ce6c57fa125bd16c8d772cc6806ac
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-06 18:28:35 +0900

    エラーメッセージの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :message, presence: true
   validates :image, presence: true
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
