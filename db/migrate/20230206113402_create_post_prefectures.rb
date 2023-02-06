b330fc48176acb3135782bc5015c78e7f9707461
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-06 20:37:35 +0900

    マイグレーションファイルの編集

--- db/migrate/20230206113402_create_post_prefectures.rb
+++ db/migrate/20230206113402_create_post_prefectures.rb
@@ -1,7 +1,8 @@
 class CreatePostPrefectures < ActiveRecord::Migration[6.0]
   def change
     create_table :post_prefectures do |t|
-
+      t.references :post,       null: false, foreign_key: true
+      t.references :prefecture, null: false, foreign_key: true
       t.timestamps
     end
   end
