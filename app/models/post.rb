56afc68b52a0710324e98cbb4f60066fc07f7158
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-06 18:39:53 +0900

    moduleを取り込む

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :prefecture
   has_many :comments, dependent: :destroy
