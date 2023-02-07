779b92b5ece98167e610383f2d5ea0cdaa71ede6
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-07 10:17:54 +0900

    修正

--- app/models/post_prefecture.rb
+++
@@ -1,6 +0,0 @@
-class PostPrefecture < ApplicationRecord
-
-  belongs_to :post
-  belongs_to :prefecture
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_115532) do
+ActiveRecord::Schema.define(version: 2023_02_06_114549) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(version: 2023_02_06_115532) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "post_prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "prefecture_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_post_prefectures_on_post_id"
-    t.index ["prefecture_id"], name: "index_post_prefectures_on_prefecture_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -100,8 +91,6 @@ ActiveRecord::Schema.define(version: 2023_02_06_115532) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "post_prefectures", "posts"
-  add_foreign_key "post_prefectures", "prefectures"
   add_foreign_key "posts", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
